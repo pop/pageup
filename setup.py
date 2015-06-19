0031408c0f670426e1393e3e91cdf50a07d2d1f8
Elijah Caine <elijahcainemv@gmail.com>
2015-06-18 17:34:52 -0700

    trying to get the python packaging to work

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from distutils.core import setup
 
-
-with open('requirements.txt') as f:
-    requirements = [l.strip() for l in f]
-
-
 setup(
     name='pageup',
-    version='0.1',
+    version='0.4',
     packages=['pageup'],
     author='Elijah Caine',
     author_email='elijahcainemv@gmail.com',
     url='https://github.com/ElijahCaine/pageup',
     description='A simple RST based static (html) page generator,',
-    install_requires=requirements,
+    install_requires=[
+                      'docutils==0.12',
+                      'Jinja2==2.7.3',
+                      'MarkupSafe==0.23',
+                      'requests==2.7.0',
+                     ],
     scripts=['pageup/pageup'],
+    data_files=[('data', ['pageup/data/template.jinja',
+                          'pageup/data/style.css',
+                          'pageup/data/content.rst'])]
 )
